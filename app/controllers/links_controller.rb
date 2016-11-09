2bd347403bb9e590ff1a00b2d3848b328d47a582
Lane Winham <mlwinham12@gmail.com>
2016-11-08 23:11:16 -0700

    working on tags but having issues with updating or altering tags

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -5,7 +5,7 @@ class LinksController < ApplicationController
   end
 
   def create
-    @link = Link.new(link_params)
+    @link = Link.new(secondary_link_params)
     if @link.save
       flash[:success] = "Successfully Created New Link!"
       redirect_to links_path
@@ -21,7 +21,8 @@ class LinksController < ApplicationController
 
   def update
     @link = Link.find(params[:id])
-    if @link.update(link_params)
+    @link.update(link_params) if link_params[:link_tags]
+    if @link.update(secondary_link_params)
       flash[:success] = "Successfully Updated!"
       redirect_to links_path
     else
@@ -33,6 +34,25 @@ class LinksController < ApplicationController
   private
 
     def link_params
-      params.require(:link).permit(:title, :url, :user_id)
+      params.require(:link).permit(:title, :url, :user_id, :tags, :link_tags)
+    end
+
+    def secondary_link_params
+      altered_params = link_params
+      if altered_params[:tags]
+        sanitize_tag(altered_params[:tags])
+      else
+        sanitize_tag(altered_params[:link_tags])
+      end
+      altered_params
+    end
+
+    def sanitize_tag(input)
+      sanitized = input.downcase.split(',').uniq
+      create_tag(sanitized)
+    end
+
+    def create_tag(tag_collection)
+      tag_collection.map { |word| Tag.find_or_create_by(name: word.strip) }
     end
 end

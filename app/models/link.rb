810702cc7c9211eacd10a5f34ce8598c250ad0ef
Lane Winham <mlwinham12@gmail.com>
2016-11-08 17:46:10 -0700

    add search term but not tested currently

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,6 +1,6 @@
 class Link < ApplicationRecord
   belongs_to :user
-  validates :url, presence: true, :url => true
+  validates :url, presence: true, url: true
   validates :title, presence: true
 
   enum read: [ :false, :true ]
